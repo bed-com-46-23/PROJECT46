487781867d35ae11ed9b762ffd08adcc077a1410
bed-com-46-23 <bed-com-46-23@unima.ac.mw>
2025-04-16 12:11:05 +0200

    Add files via upload

---
+++ Globalvariable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//grobal variable declaration
+int g = 20;
+int main(){
+    //local variable declaration
+    int g = 10;
+    cout<<g;
+    return 0;
+}
---
+++ UserInput.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+using namespace std;
+int main() {
+    int x,y;
+    int sum;
+    cout<<"enter a number: ";
+    cin>>x;
+    cout<<"enter another number: ";
+    cin>>y;
+    sum = x + y;
+    cout<<"the sum is: "<<sum;
+    return 0;
+
+}
